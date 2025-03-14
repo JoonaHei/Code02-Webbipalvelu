1bf7cf85d33dd12792c03c2414526f4320adf516
Joona Heiskanen <Joona.Heiskanen@edu.mayk.fi>
2025-03-14 11:50:53 +0200

    painikkeen lisäys (5€)

--- Webbipalvelu/palvelu.py
+++ Webbipalvelu/palvelu.py
@@ -4,17 +4,29 @@ Itse koodi alkaa tästä
 
 Tämä on Osa Code03 kurssia 13.3.2025
 """
-
 def app(environ, respond):
     respond('200 OK', [('Content-type', 'text/html; charset=utf-8')])
-    yield "Älä katso tänne!".encode('utf-8')
-    polku = environ["PATH_INFO"].strip('/')
-    salanimi = polku.replace("a","aca").replace("i", "hani").replace("n","Nano")
-    yield "<p>moikka<p>".encode('utf-8')
-    yield (f"<BR>Salainen nimesi on: <b>{salanimi}</b>".encode('utf-8'))
-    #for key in environ:
-    #    yield ("%s: %s\n" % (key, environ[key])).encode('utf-8')
+    
+    # Tarkistetaan, onko nappia painettu
+    query_string = environ.get("QUERY_STRING", "")
+    nappi_painettu = "napin_arvo=1" in query_string  # Tarkistaa, onko lomake lähetetty
 
+    yield "<p>Älä katso tänne!</p>".encode('utf-8')
+
+    polku = environ.get("PATH_INFO", "").strip('/')
+    salanimi = polku.replace("a", "aca").replace("i", "hani").replace("n", "Nano")
+
+    yield f"<p>Salainen nimesi on: <b>{salanimi}</b></p>".encode('utf-8')
+
+    if nappi_painettu:
+        yield "<p><b>Oikeasti! Saat 5€! (tai sitten et)</b></p>".encode('utf-8')
+    else:
+        yield """
+        <form method="GET">
+            <input type="hidden" name="napin_arvo" value="1">
+            <input type="submit" value="Paina niin saat 5€">
+        </form>
+        """.encode("utf-8")
 
 if __name__ == '__main__':
     with make_server("localhost", 8000, app) as server: 
